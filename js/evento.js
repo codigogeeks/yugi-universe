8f9b9bf73a181ac6b2bce3e68f3ad916c64dc59c
Gustavo Saturnino <ga38185@gmail.com>
2023-09-15 19:57:28 -0300

    Cards

--- js/evento.js
+++ js/evento.js
@@ -9,3 +9,31 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	formulario.classList.remove("right-panel-active");
 });
+
+const card1 = document.querySelector('.card1__inner');
+const card2 = document.querySelector('.card2__inner');
+const card3 = document.querySelector('.card3__inner');
+
+card1.addEventListener("mouseover", () => {
+	card1.classList.toggle('is-flipped');
+});
+
+card1.addEventListener("mouseout", () => {
+	card1.classList.toggle('is-flipped');
+});
+
+card2.addEventListener("mouseover", () => {
+	card2.classList.toggle('is-flipped');
+});
+
+card2.addEventListener("mouseout", () => {
+	card2.classList.toggle('is-flipped');
+});
+
+card3.addEventListener("mouseover", () => {
+	card3.classList.toggle('is-flipped');
+});
+
+card3.addEventListener("mouseout", () => {
+	card3.classList.toggle('is-flipped');
+});
