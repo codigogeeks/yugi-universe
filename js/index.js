908f8044f11dd090e43e9841bea6062a783cfa5f
Gabriella Hernandes <gbhernandes@hotmail.com>
2023-09-16 23:41:45 -0300

    index

--- js/index.js
+++ js/index.js
@@ -2,18 +2,18 @@ let x = 0;
 let p = -10;
 let i = 1;
 let doom = document.getElementsByClassName('item');
-let confr = (doom.length - 3)*(-350);
+let confr = (doom.length - 3)*(-280);
 let confl = 0;
 //left
 document.getElementById("l").addEventListener("click", function(){
-       x += 350;
-       if (x > confl){x = confr - 350}else{
+       x += 280;
+       if (x > confl){x = confr - 280}else{
     document.getElementById("move").style.marginLeft = x + "px";}
 });
 //right
 document.getElementById("r").addEventListener("click", function(){
-    x += -350;
+    x += -280;
     p += -10;
-    if (x < confr){x = 350}else{
+    if (x < confr){x = 280}else{
     document.getElementById("move").style.marginLeft = x + "px";}
 });
